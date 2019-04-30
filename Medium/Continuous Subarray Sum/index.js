a02695fc7802900d8340f77ff9a100ae04b0f3aa
GQSM <ms314006@hotmail.com>
2019-04-30 09:50:55 +0800

    [CHANGE] Rename tempK to grandTotalK

--- Medium/Continuous Subarray Sum/index.js
+++ Medium/Continuous Subarray Sum/index.js
@@ -5,26 +5,26 @@
  */
 const checkSubarraySum = (nums, k) => {
     let tempNums = nums;
-    let tempK = 0;
+    let grandTotalK = 0;
 
     // If array length greater than 1, then continue calculation.
     while (tempNums.length > 1) {
-        tempK = tempNums[0];
+        grandTotalK = tempNums[0];
         for (let i = 1; i <= tempNums.length - 1; i += 1) {
-            tempK += tempNums[i];
+            grandTotalK += tempNums[i];
 
             // If k is 0, then the contiguous subarray is added to 0, returning true.
             if (k === 0) {
-                if (tempK === 0) {
+                if (grandTotalK === 0) {
                     return true;
                 };
 
             // If k not 0, then use % check if it can be divisible.
-            } else if (tempK % k === 0) {
+            } else if (grandTotalK % k === 0) {
                 return true;
             };
         };
-        tempK = 0;
+        grandTotalK = 0;
         // Delete index 0 in the array before the next loop.
         tempNums.splice(0, 1);
     };
